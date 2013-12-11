a4a51bbd3155d26aefb6df7032ae879ed3e7d531
xion <shayan.roychoudhury@gmail.com>
2013-12-11 23:42:51 +0530

    Removed example code

--- NeuralNet3/src/ThrustTest.cu
+++ NeuralNet3/src/ThrustTest.cu
@@ -11,23 +11,6 @@
 #include <thrust/host_vector.h>
 #include <thrust/reduce.h>
 
-static const int WORK_SIZE = 256;
-
-__host__ __device__ unsigned int bitreverse(unsigned int number)
-{
-	number = ((0xf0f0f0f0 & number) >> 4) | ((0x0f0f0f0f & number) << 4);
-	number = ((0xcccccccc & number) >> 2) | ((0x33333333 & number) << 2);
-	number = ((0xaaaaaaaa & number) >> 1) | ((0x55555555 & number) << 1);
-	return number;
-}
-
-struct bitreverse_functor
-{
-	__host__ __device__ unsigned int operator()(const unsigned int &x)
-	{
-		return bitreverse(x);
-	}
-};
 
 int main()
 {
