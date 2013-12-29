2ef2c60a0b9b4439195b26ba31f5cc68469adf32
xion <shayan.roychoudhury@gmail.com>
2013-12-29 14:05:52 +0530

    Added randomized weight initialization

--- NeuralNet3/src/Neuron.cu
+++ NeuralNet3/src/Neuron.cu
@@ -7,11 +7,23 @@
 
 #include "Neuron.h"
 
+
+struct randomize
+	{
+		__host__  void operator()( double &x ) const {
+		x=(double) (rand() % 1000000) / 1000000 - 0.5;
+
+	}
+};
+
 	Neuron::Neuron(int no_of_inputs)
 	{
 		d_weightVector=thrust::device_vector<double>(no_of_inputs);
 		h_weightVector=thrust::host_vector<double>(no_of_inputs);
 
+		//Init weights to random numbers between 0.5 and -0.5 using the randomize functor
+		thrust::for_each(h_weightVector.begin(),h_weightVector.end(),randomize());
+		d_weightVector=h_weightVector;
 		d_momentum=	thrust::device_vector<double>(no_of_inputs,0);
 		h_momentum=	thrust::host_vector<double>(no_of_inputs,0);;
 	}
