e563e5670f87fe0cbb68cf8a817cee1cc2990353
xion <shayan.roychoudhury@gmail.com>
2014-01-24 12:11:17 +0530

    Rethink of constructor (common init function) Moved weights to neuron Added propagateOutput Rethought propagateBack Rethink of getters and setters

--- NeuralNet3/src/Layer.h
+++ NeuralNet3/src/Layer.h
@@ -15,7 +15,10 @@
 #include <thrust/transform.h>
 #include <thrust/functional.h>
 #include <vector>
+#include "utils.h"
+
 #include "Neuron.h"
+
 
 class Layer {
 	int no_of_inputs;
@@ -25,18 +28,22 @@ class Layer {
 	double momentumConstant;
 
 	thrust::device_vector<double> d_inputs;
-	thrust::device_vector<double> d_weights;
 	thrust::device_vector<double> d_outputs;
 	thrust::device_vector<double> d_desired_outputs;
 	thrust::device_vector<double> d_dW;
 
 	std::vector<Neuron> neurons;
+
+	void init(int input_size, int output_size);
+
 public:
 	Layer(int input_size, int output_size);
 	Layer(int input_size, int output_size,double _learningRate,double _momentumConstant);
 
+	void randomizeWeights();
+
 	thrust::host_vector<double> h_inputs;
-	thrust::host_vector<double> h_weights;
+	//thrust::host_vector<double> h_weights;
 	thrust::host_vector<double> h_outputs;
 	thrust::host_vector<double> h_desired_outputs;
 	thrust::host_vector<double> h_dW;
@@ -51,9 +58,21 @@ public:
 
 
 	void feedForward();
+	void propagateOutput(Layer &layer);
 	void calculateError();
-	void propagateBack(thrust::device_vector<double>);
+	void propagateBack(Layer &layer);
 	void modifyweights();
+
+	void setInputs(std::vector<double> inputs);
+	void setInputs(thrust::host_vector<double> inputs);
+	void setDesiredOutputs(std::vector<double> outputs);
+
+
+	std::vector<double> getDesiredOutputs();
+	std::vector<double> getOutputs();
+	std::vector<double> getinputs();
+	std::vector<double> getError();
+
 };
 
 #endif /* LAYER_H_ */
