ad49119382ff44d11e05fb713301465460534ea8
xion <shayan.roychoudhury@gmail.com>
2014-01-26 21:43:45 +0530

    initial commit of CSVWriter

---
+++ NeuralNet3/src/IOUtils/CSVWriter.cpp
@@ -0,0 +1,32 @@
+/*
+ * CSVWriter.cpp
+ *
+ *  Created on: 18-Jan-2014
+ *      Author: xion
+ */
+
+#include "CSVWriter.h"
+
+CSVWriter::CSVWriter(std::string CSVFileName)
+{
+ CSVFile.open(CSVFileName.c_str(),std::ios::trunc);
+}
+
+
+/*template <class T>
+bool CSVWriter::write(std::vector<T> data)
+{
+	bool writeSuccesfull=true;
+
+	for (unsigned i = 0; i < data.size(); i++) {
+			CSVFile << data.at(i) << ",";
+		}
+	CSVFile<<"\n";
+	return writeSuccesfull;
+}*/
+
+CSVWriter::~CSVWriter()
+{
+	std::cout<<"CSVWriter called!!";
+	CSVFile.close();
+}
