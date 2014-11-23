43660aa15c5f82923b27774b6d053d9d99a35bb7
xion <xion@LucidSphere>
2014-11-23 19:09:49 +0530

    Changed implementation of printvector to use for loops instead of std::copy directly to output stream

--- NeuralNet3/src/IOUtils/utils.cu
+++ NeuralNet3/src/IOUtils/utils.cu
@@ -2,21 +2,42 @@
 
 void printVector(std::vector<double> vec)
 {
-	std::copy(vec.begin(), vec.end(), std::ostream_iterator<double>(std::cout, ","));
+
+
+	for(std::vector<double>::iterator it=vec.begin();it!=vec.end();it++)
+	{
+		std::cout<<"["<<*it<<"] ";
+	}
+
+	std::cout<<"\n";
+}
+
+void printVector(std::vector<double> vec, std::string separator)
+{
+	for(std::vector<double>::iterator it=vec.begin();it!=vec.end();it++)
+	{
+		std::cout<<*it<<separator;
+	}
 
 	std::cout<<"\n";
 }
 
 void printVector(thrust::host_vector<double> vec)
 {
-	thrust::copy(vec.begin(), vec.end(), std::ostream_iterator<double>(std::cout, ","));
+		for(thrust::host_vector<double>::iterator it=vec.begin();it!=vec.end();it++)
+	{
+		std::cout<<"["<<*it<<"] ";
+	}
 
 	std::cout<<"\n";
 }
 
 void printVector(thrust::device_vector<double> vec)
 {
-	thrust::copy(vec.begin(), vec.end(), std::ostream_iterator<double>(std::cout, ","));
+	for(thrust::device_vector<double>::iterator it=vec.begin();it!=vec.end();it++)
+	{
+		std::cout<<"["<<*it<<"] ";
+	}
 
 	std::cout<<"\n";
 }
--- NeuralNet3/src/IOUtils/utils.h
+++ NeuralNet3/src/IOUtils/utils.h
@@ -13,8 +13,12 @@
 #include <thrust/host_vector.h>
 #include <vector>
 
+//helper functions to print vectors
 void printVector(std::vector<double> vec);
+void printVector(std::vector<double> vec, std::string separator);
 
+
+//helper functions to print host & device vectors
 void printVector(thrust::host_vector<double> vec);
 void printVector(thrust::device_vector<double> vec);
 
